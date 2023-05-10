2e0291493f81b9eda17f2a6d32e638f8cfa15ee4
Diana Baghirova <109644019+d-baghirova@users.noreply.github.com>
2023-05-10 19:30:36 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 const fs = require('fs');
 const express = require('express');
 
-//Cheetsheet
-//let today = Date();
-//let format = today.toLocaleDateString('en-US');
-
-
-
-
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -39,6 +32,50 @@ const showOthers = (namee) => {
     }
     return others;
 }
+function pad(n){return n<10 ? '0'+n : n}
+function ISODateString(today){
+    return pad(today.getUTCHours())+':'
+    + pad(today.getUTCMinutes())+':'
+    + pad(today.getUTCSeconds())
+}
+
+const getTime1 = () => {
+    const today = new Date();
+    const format = '|'+today.toLocaleDateString('en-US');
+    const hours = pad(today.getHours())+':';
+    const minutes = pad(today.getMinutes())+':';
+    const seconds = pad(today.getSeconds())+':';
+    const time = hours+minutes+seconds;
+    return time;
+}
+
+const showMessages = (chatName) => {
+    const file = JSON.parse(fs.readFileSync('./chats.json', 'utf-8'));
+    const messages = file.chats[chatName];
+    let mess = []
+    
+    for (let m of messages){
+        const val = Object.values(m);
+        const k = Object.keys(m);
+        if (val.length){
+            let arr = k[0].split(' ');
+            let n = arr[3];
+            mess.push([val[0],n]);
+        }
+    }
+
+    return mess;
+}
+
+const getTime = () => {
+    const today = new Date();
+    const format = '|'+today.toLocaleDateString('en-US');
+    const hours = today.getHours()+':';
+    const minutes = today.getMinutes()+':';
+    const seconds = today.getSeconds()+':';
+    const time = hours+minutes+seconds;
+    return time;
+}
 
 
 app.get('/user/:username/', (req, res) => {
@@ -47,12 +84,38 @@ app.get('/user/:username/', (req, res) => {
 });
 
 app.get('/user/:username/chat/:friend', (req, res, next) => {
-    let data = {username: req.params.username, friend: req.params.friend ,others: showOthers(req.params.username)};
+    let data = {username: req.params.username, friend: req.params.friend, message: req.params.message, others: showOthers(req.params.username),};
+    let file = JSON.parse(fs.readFileSync('./chats.json', 'utf-8'));
+    let chs = file.chats;
+
+    let chat = data.username + "-" + data.friend + "-chat";
+    let chat1 = data.friend + "-" + data.username + "-chat";
+
+    let time = getTime();
+    let mmname = `message/${time}`;
+    let mname = `${time}`+' Message from '+data.username;
+    let message = {[mname]: file[mmname]};
+    let to = `to-${data.friend}`;
+    let to1 = `to-${data.username}`;
+    
+    if (chat in chs){
+        file.chats[chat].push(message);
+        fs.writeFileSync('./chats.json', JSON.stringify(file, null, 2));
+        data.mess = showMessages(chat);
+    } else if (chat1 in chs){
+        file.chats[chat1].push(message);
+        fs.writeFileSync('./chats.json', JSON.stringify(file, null, 2));
+        data.mess = showMessages(chat1);
+    } else if (!(chat in chs && chat1 in chs)) {
+        file.chats[chat] = [];
+        fs.writeFileSync('./chats.json', JSON.stringify(file, null, 2));
+        data.mess = showMessages(chat);
+    }
     res.render('chat', data);
 })
 
 app.get('/user/:username/chat/:friend/:message', (req, res) => {
-    let data = {username: req.params.username, friend: req.params.friend ,others: showOthers(req.params.username)};
+    let data = {username: req.params.username, friend: req.params.friend, message: req.params.message, others: showOthers(req.params.username)};
     res.render('chat', data);
 })
 
@@ -73,25 +136,15 @@ app.post('/check-user', (req, res) => {
     }
 });
 
-app.post('/send', (req, res, next) => {
+app.post('/user/:username/chat/:friend/:message', (req, res, next) => {
     const m = req.body.message;
-    const name = req.body.username;
-    const friend = req.body.friend;
-    let today = new Date();
-    let format = '/'+today.toLocaleDateString('en-US');
-    let hours = today.getHours()+':';
-    let minutes = today.getMinutes()+':';
-    let seconds = today.getSeconds()+':';
-    let time = hours+minutes+seconds;
-    let mname = 'message'+time+format;
 
-    let message = {[mname]: m};
+    let file = JSON.parse(fs.readFileSync('./chats.json', 'utf-8'));
+    let time = getTime();
+    file[`message/${time}`] = m;
 
-    let file = JSON.parse(fs.readFileSync('./names.json', 'utf-8'));
-    file.messages.push(message);
-
-
-    fs.writeFileSync('./names.json', JSON.stringify(file, null, 2));
+    fs.writeFileSync('./chats.json', JSON.stringify(file, null, 2));
+    
     return res.redirect(req.get(`referer`));
 })
 
@@ -102,7 +155,7 @@ app.post('/verify', (req, res) => {
     let obj = {name: username, password: word};
 
     let file = JSON.parse(fs.readFileSync('./names.json', 'utf-8'));
-    let userss = file.users
+    let userss = file.users;
     let arr = [];
 
     for (i of userss){
